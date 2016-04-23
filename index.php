4f014cc965d39ef6b5244cdcd44c1bc107ed5643
muhfadhiil26 <muhfadhiil@gmail.com>
2016-04-23 07:23:33 +0700

    merubah index.php

--- index.php
+++ index.php
@@ -1,112 +1,110 @@
 <?php
-echo "<!DOCTYPE html>\n";
-echo "<html lang=\"en\">\n";
-echo "<head>\n";
-echo "  <title> BedahBola </title>\n";
-echo "  <link rel=\"stylesheet\" href=\"style.css\">\n";
-echo "  <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>\n";
-echo "  <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">\n";
-echo "\n";
-echo "  <script>\n";
-echo "    function toggleMenu() {\n";
-echo "      var menu = document.getElementById('menu-3');\n";
-echo "      menu.classList.toggle('menu-open');\n";
-echo "    }\n";
-echo "  </script>\n";
-echo "\n";
-echo "</head>\n";
-echo "\n";
-echo "<body>\n";
-echo "  <div id=\"pertama\">\n";
-echo "\n";
-echo "    <div id=\"header\">\n";
-echo "      <img src=\"asset/Soccer-icon.png\" id=\"logo\" alt=\"\">\n";
-echo "      <div id=\"menu\">\n";
-echo "        <img src=\"asset/1.jpg\" id=\"nav-icon\" alt=\"\" onclick=\"toggleMenu();\">\n";
-echo "        <div id=\"menu-text\" onclick=\"toggleMenu();\">Menu</div>\n";
-echo "        <div class=\"konten\" id=\"menu-3\">\n";
-echo "            <a href=\"#pertama\" class=\"kolom_3\">Home</a>\n";
-echo "            <a href=\"#kedua\" class=\"kolom_3 kolom_3_tengah\" onclick=\"return smoothScroll('kedua')\">Artikel</a>\n";
-echo "            <a href=\"#ketiga\" class=\"kolom_3\" onclick=\"return smoothScroll('ketiga')\">Kontak</a>\n";
-echo "        </div>\n";
-echo "      </div>\n";
-echo "    </div>\n";
-echo "\n";
-echo "    <div class=\"clear\"></div>\n";
-echo "\n";
-echo "    <div class=\"konten\">\n";
-echo "\n";
-echo "      <div id=\"SD\">\n";
-echo "        <h1>Selamat datang di <br> website <br> bedahbola.com</h1>\n";
-echo "        <a href=\"#kedua\" onclick=\"return smoothScroll('kedua')\"> Artikel </a>\n";
-echo "      </div>\n";
-echo "\n";
-echo "    </div>\n";
-echo "  </div>\n";
-echo "\n";
-echo "  <div id=\"kedua\">\n";
-echo "    <div class=\"konten\">\n";
-echo "      <h2> PILIHAN ARTIKEL </h2>\n";
-echo "\n";
-echo "      <div class=\"kolom_3\">\n";
-echo "        <a href=\"bola.php\">\n";
-echo "          <div class=\"gambar_artikel\">\n";
-echo "            <img src=\"asset/bola1.jpg\" alt=\"\" />\n";
-echo "            <p> Sepak Bola </p>\n";
-echo "          </div>\n";
-echo "        </a>\n";
-echo "      </div>\n";
-echo "\n";
-echo "      <div class=\"kolom_3 kolom_3_tengah\">\n";
-echo "        <a href=\"futsal.php\">\n";
-echo "          <div class=\"gambar_artikel\">\n";
-echo "            <img src=\"asset/futsal.jpg\" alt=\"\" />\n";
-echo "            <p> Futsal </p>\n";
-echo "          </div>\n";
-echo "        </a>\n";
-echo "      </div>\n";
-echo "\n";
-echo "      <div class=\"kolom_3\">\n";
-echo "        <a href=\"setiap_artikel.html\">\n";
-echo "          <div class=\"gambar_artikel\">\n";
-echo "            <img src=\"asset/bola.jpg\" alt=\"\" />\n";
-echo "            <p> Analisa Pertandingan </p>\n";
-echo "          </div>\n";
-echo "        </a>\n";
-echo "      </div>\n";
-echo "\n";
-echo "      <div class=\"clear\"></div>\n";
-echo "    </div>\n";
-echo "  </div>\n";
-echo "\n";
-echo "  <div id=\"ketiga\">\n";
-echo "    <div class=\"konten\">\n";
-echo "      <h2> Kontak Kami </h2>\n";
-echo "\n";
-echo "      <div class=\"kolom_2\">\n";
-echo "        <form action=\"\" novalidate onsubmit=\"return validasi(this)\">\n";
-echo "          <input type=\"text\" name=\"nama\" placeholder=\"Nama\">\n";
-echo "          <input type=\"email\" name=\"email\" placeholder=\"Email\">\n";
-echo "          <textarea name=\"pesan\" placeholder=\"Kritik & Saran\" rows=\"8\" cols=\"40\"></textarea>\n";
-echo "          <input type=\"submit\" name=\"submit\" placeholder=\"Kirim\">\n";
-echo "        </form>\n";
-echo "      </div>\n";
-echo "\n";
-echo "      <div class=\"kolom_2\">\n";
-echo "        <iframe src=\"https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d253840.478949799!2d106.829518!3d-6.2297465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1460786524864\" width=\"600\" height=\"250\" frameborder=\"0\" style=\"border:0\" allowfullscreen></iframe>\n";
-echo "      </div>\n";
-echo "\n";
-echo "        <div class=\"clear\"></div>\n";
-echo "    </div>\n";
-echo "  </div>\n";
-echo "\n";
-echo "  <div id=\"footer\">\n";
-echo "    <p> &copy; BedahBola 2016 </p>\n";
-echo "  </div>\n";
-echo "\n";
-echo "<script src=\"bedahbola.js\"> </script>\n";
-echo "\n";
-echo "</body>\n";
-echo "</html>\n";
-echo "\n";
+require_once "core/init.php";
 ?>
+
+<head>
+  <title> BedahBola </title>
+  <link rel="stylesheet" href="style.css">
+  <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <script>
+    function toggleMenu() {
+      var menu = document.getElementById('menu-3');
+      menu.classList.toggle('menu-open');
+    }
+  </script>
+
+</head>
+
+<body>
+  <div id="pertama">
+
+    <div id="header">
+      <img src="asset/Soccer-icon.png" id="logo" alt="">
+      <div id="menu">
+        <img src="asset/1.jpg" id="nav-icon" alt="" onclick="toggleMenu();">
+        <div id="menu-text" onclick="toggleMenu();">Menu</div>
+        <div class="konten" id="menu-3">
+            <a href="#pertama" class="kolom_3">Home</a>
+            <a href="#kedua" class="kolom_3 kolom_3_tengah" onclick="return smoothScroll('kedua')">Artikel</a>
+            <a href="#ketiga" class="kolom_3" onclick="return smoothScroll('ketiga')">Kontak</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="clear"></div>
+
+    <div class="konten">
+
+      <div id="SD">
+        <h1>Selamat datang di <br> website <br> bedahbola.com</h1>
+        <a href="#kedua" onclick="return smoothScroll('kedua')"> Artikel </a>
+      </div>
+
+    </div>
+  </div>
+
+  <div id="kedua">
+    <div class="konten">
+      <h2> PILIHAN ARTIKEL </h2>
+
+      <div class="kolom_3">
+        <a href="bola.php">
+          <div class="gambar_artikel">
+            <img src="asset/bola1.jpg" alt="" />
+            <p> Sepak Bola </p>
+          </div>
+        </a>
+      </div>
+
+      <div class="kolom_3 kolom_3_tengah">
+        <a href="setiap_artikel.html">
+          <div class="gambar_artikel">
+            <img src="asset/futsal.jpg" alt="" />
+            <p> Futsal </p>
+          </div>
+        </a>
+      </div>
+
+      <div class="kolom_3">
+        <a href="setiap_artikel.html">
+          <div class="gambar_artikel">
+            <img src="asset/bola.jpg" alt="" />
+            <p> Analisa Pertandingan </p>
+          </div>
+        </a>
+      </div>
+
+      <div class="clear"></div>
+    </div>
+  </div>
+
+  <div id="ketiga">
+    <div class="konten">
+      <h2> Kontak Kami </h2>
+
+      <div class="kolom_2">
+        <form action="" novalidate onsubmit="return validasi(this)">
+          <input type="text" name="nama" placeholder="Nama">
+          <input type="email" name="email" placeholder="Email">
+          <textarea name="pesan" placeholder="Kritik & Saran" rows="8" cols="40"></textarea>
+          <input type="submit" name="submit" placeholder="Kirim">
+        </form>
+      </div>
+
+      <div class="kolom_2">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d253840.478949799!2d106.829518!3d-6.2297465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1460786524864" width="600" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+        <div class="clear"></div>
+    </div>
+  </div>
+
+  <div id="footer">
+    <p> &copy; BedahBola 2016 </p>
+  </div>
+
+<script src="bedahbola.js"> </script>
+
+</body>
